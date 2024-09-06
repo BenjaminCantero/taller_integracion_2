414bc8f64b7ca55d3a2ea1a2313b0e6b66fa617a
oscarbarra <135163378+oscarbarra@users.noreply.github.com>
2024-09-05 20:50:14 -0400

    Paleta de Colores Personalizada + Maquetado Inicial

--- gistocked/tailwind.config.js
+++ gistocked/tailwind.config.js
@@ -7,11 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      /*
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      }, 
+      */
+      
+      colors: {
+        PaletaLogin: {
+          1: "#DEE2E6",
+          2: "#343A40",
+          3: "#E9ECEF"
+        }
+      }
     },
   },
   plugins: [],
