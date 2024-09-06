2ecc459499309f5da788db5ee41ee564caebb931
oscarbarra <135163378+oscarbarra@users.noreply.github.com>
2024-09-05 22:03:16 -0400

    Estilos Personalizados + Maquetado v0.2

--- gistocked/src/app/page.js
+++ gistocked/src/app/page.js
@@ -2,14 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    
-  <body className="grid grid-cols-3 bg-PaletaLogin-1">
-    <main>
-      <div className="col-start-2 col-end-3">
-
-      </div>
-    </main>
-  </body>
-
+    <div className="grid grid-cols-3 grid-rows-3 gap-3 bg-PaletaLogin-1">
+      <main className="col-start-2 col-end-3 row-start-2 row-end-3">
+          <div className="bg-PaletaLogin-2 rounded-lg">
+              <p>Hola Mundo</p>
+          </div>
+      </main>
+    </div>
   );
 }
