83409d6dfb8197beae1ee7160f73c5dce743ca76
Gerald-herrerab <134649587+Gerald-herrerab@users.noreply.github.com>
2024-09-07 23:41:21 -0400

    redireccion

---
+++ gistocked/src/app/Grafics.js
@@ -0,0 +1,8 @@
+export default function Grafics() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-gray-800">Estadísticas de la Tienda</h1>
+      </div>
+    );
+  }
+  
