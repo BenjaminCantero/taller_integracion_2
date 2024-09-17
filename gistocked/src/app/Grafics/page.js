73aa09d1c06a44c98187eaa6a517ea63be3dac27
Gerald-herrerab <134649587+Gerald-herrerab@users.noreply.github.com>
2024-09-16 23:08:43 -0300

    Sidebar en proceso

--- gistocked/src/app/Grafics/page.js
+++ gistocked/src/app/Grafics/page.js
@@ -17,31 +17,37 @@ export default function Home() {
 
   return (
     <div>
-        {/* Aquí incluiré las gráficas (un componente por cada ejemplo). */}
-        <h1 className="bg-info text-center font-monospace fw-bold lh-base">Gráficas ChartJS</h1>
-        <div>
-            <p className="m-2"><b>Ejemplo #1: </b>Gráfico de líneas básico</p>
-            <div className="bg-light mx-auto px-2 border border-2 border-primary" style={{width:"450px", height:"230px"}}>
-              <LinesChart />
-            </div>
+      <div className="flex">
+        <div className="w-72 h-screen bg-dark-purple">Sidebar</div>
+        <div className="p-7 text-2x1 font-semibold">
+          <h1>Home page</h1>
+          {/* Aquí incluiré las gráficas (un componente por cada ejemplo). */}
+              <div>
+                  <p className="m-2"><b>Ejemplo #1: </b>Gráfico de líneas básico</p>
+                  <div className="bg-light mx-auto px-2 border border-2 border-primary" style={{width:"450px", height:"230px"}}>
+                    <LinesChart />
+                  </div>
+              </div>
+              <hr className="mt-3 mb-2"/>
+              <div>
+                  <p className="m-2"><b>Ejemplo #2: </b>Gráfico de barras</p>
+                  <div className="bg-light mx-auto px-2 border border-2 border-primary" style={{width:"450px", height:"225px"}}>
+                    <BarsChart />
+                  </div>
+              </div>
+              <hr className="mt-3 mb-2"/>
+              <div>
+                  <p className="m-2"><b>Ejemplo #3: </b>Gráfico circular</p>
+                  <div className="bg-light mx-auto border border-2 border-primary" style={{width:"450px", height:"250px"}}>
+                      <div style={{width:"100%", height:"100%", padding:"10px 0"}}>
+                        <PiesChart />             
+                      </div>
+                  </div>
+              </div>
+          </div>
         </div>
-        <hr className="mt-3 mb-2"/>
-        <div>
-            <p className="m-2"><b>Ejemplo #2: </b>Gráfico de barras</p>
-            <div className="bg-light mx-auto px-2 border border-2 border-primary" style={{width:"450px", height:"225px"}}>
-              <BarsChart />
-            </div>
-        </div>
-        <hr className="mt-3 mb-2"/>
-        <div>
-            <p className="m-2"><b>Ejemplo #3: </b>Gráfico circular</p>
-            <div className="bg-light mx-auto border border-2 border-primary" style={{width:"450px", height:"250px"}}>
-                <div style={{width:"100%", height:"100%", padding:"10px 0"}}>
-                  <PiesChart />             
-                </div>
-            </div>
-        </div>
-    </div>
+      </div>
+
   );
 
 
