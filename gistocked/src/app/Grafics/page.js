50ed7197bc518be8df3263387f8c2750c86d7257
Gerald-herrerab <134649587+Gerald-herrerab@users.noreply.github.com>
2024-09-17 00:30:26 -0300

    actualizacion Sidebar

--- gistocked/src/app/Grafics/page.js
+++ gistocked/src/app/Grafics/page.js
@@ -2,51 +2,99 @@
 
 import { useState, useEffect, useRef } from "react";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import LinesChart from "./LinesChart";
 import BarsChart from "./BarsChart";
 import PiesChart from "./PiesChart";
 
 
-
-
 export default function Home() {
   const chartRef = useRef(null);
   const chartInstanceRef = useRef(null);
+  const [open, setOpen] = useState(true);
+  const Menus = [
+    { title: "Dashboard", src: "Chart_fill" },
+    { title: "Inbox", src: "Chat" },
+    { title: "Accounts", src: "User", gap: true },
+    { title: "Schedule ", src: "Calendar" },
+    { title: "Search", src: "Search" },
+    { title: "Analytics", src: "Chart" },
+    { title: "Files ", src: "Folder", gap: true },
+    { title: "Setting", src: "Setting" },
+  ];
 
   return (
     <div>
       <div className="flex">
-        <div className="w-72 h-screen bg-dark-purple">Sidebar</div>
-        <div className="p-7 text-2x1 font-semibold">
-          <h1>Home page</h1>
-          {/* Aquí incluiré las gráficas (un componente por cada ejemplo). */}
-              <div>
-                  <p className="m-2"><b>Ejemplo #1: </b>Gráfico de líneas básico</p>
-                  <div className="bg-light mx-auto px-2 border border-2 border-primary" style={{width:"450px", height:"230px"}}>
-                    <LinesChart />
-                  </div>
-              </div>
-              <hr className="mt-3 mb-2"/>
-              <div>
-                  <p className="m-2"><b>Ejemplo #2: </b>Gráfico de barras</p>
-                  <div className="bg-light mx-auto px-2 border border-2 border-primary" style={{width:"450px", height:"225px"}}>
-                    <BarsChart />
-                  </div>
-              </div>
-              <hr className="mt-3 mb-2"/>
-              <div>
-                  <p className="m-2"><b>Ejemplo #3: </b>Gráfico circular</p>
-                  <div className="bg-light mx-auto border border-2 border-primary" style={{width:"450px", height:"250px"}}>
-                      <div style={{width:"100%", height:"100%", padding:"10px 0"}}>
-                        <PiesChart />             
-                      </div>
-                  </div>
-              </div>
-          </div>
+      <div
+        className={` ${open ? "w-72" : "w-20 "
+        } bg-dark-purple h-screen p-5  pt-8 relative duration-300`}
+      >
+        <img
+          src="./assets/control.png"
+          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+           border-2 rounded-full  ${!open && "rotate-180"}`}
+          onClick={() => setOpen(!open)}
+        />
+        <div className="flex gap-x-4 items-center">
+          <img
+            src="./assets/logo.png"
+            className={`cursor-pointer duration-500 ${
+              open && "rotate-[360deg]"
+            }`}
+          />
+          <h1
+            className={`text-white origin-left font-medium text-xl duration-200 ${
+              !open && "scale-0"
+            }`}
+          >
+            Designer
+          </h1>
+        </div>
+        <ul className="pt-6">
+          {Menus.map((Menu, index) => (
+            <li
+              key={index}
+              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
+              ${Menu.gap ? "mt-9" : "mt-2"} ${
+                index === 0 && "bg-light-white"
+              } `}
+            >
+              <img src={`./assets/${Menu.src}.png`} />
+              <span className={`${!open && "hidden"} origin-left duration-200`}>
+                {Menu.title}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="p-7 text-2x1 font-semibold">
+        <h1>Home page</h1>
+        {/* Aquí incluiré las gráficas (un componente por cada ejemplo). */}
+            <div>
+                <p className="m-2"><b>Ejemplo #1: </b>Gráfico de líneas básico</p>
+                <div className="bg-light mx-auto px-2 border border-2 border-primary" style={{width:"450px", height:"230px"}}>
+                  <LinesChart />
+                </div>
+            </div>
+            <hr className="mt-3 mb-2"/>
+            <div>
+                <p className="m-2"><b>Ejemplo #2: </b>Gráfico de barras</p>
+                <div className="bg-light mx-auto px-2 border border-2 border-primary" style={{width:"450px", height:"225px"}}>
+                  <BarsChart />
+                </div>
+            </div>
+            <hr className="mt-3 mb-2"/>
+            <div>
+                <p className="m-2"><b>Ejemplo #3: </b>Gráfico circular</p>
+                <div className="bg-light mx-auto border border-2 border-primary" style={{width:"450px", height:"250px"}}>
+                    <div style={{width:"100%", height:"100%", padding:"10px 0"}}>
+                      <PiesChart />             
+                    </div>
+                </div>
+            </div>
         </div>
       </div>
+    </div>
 
   );
 
