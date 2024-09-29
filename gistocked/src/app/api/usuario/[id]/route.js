4a961da4f173ac0e8759fb8cc4268b7fb62ff56f
oscarbarra <135163378+oscarbarra@users.noreply.github.com>
2024-09-29 20:54:23 -0300

    Gestion de Usuarios

--- gistocked/src/app/api/usuario/[id]/route.js
+++ gistocked/src/app/api/usuario/[id]/route.js
@@ -2,12 +2,13 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request, { params }) {
-    const nombre = params.id;
+    const rol = parseInt(params.id);
 
     try {
-        const usuario = await prisma.usuario.findFirst({
-            where: { nombre: nombre },
+        const usuario = await prisma.usuario.findMany({
+            where: { rolId: rol },
         });
+
         if (!usuario){
             return new NextResponse("Usuario NO Encontrado", { status: 404 })
         }
@@ -18,34 +19,47 @@ export async function GET(request, { params }) {
 }
 
 export async function DELETE(request, { params }) {
-    const nombre = params.id
+    const id = parseFloat(params.id)
 
     try{
         const result = await prisma.usuario.delete({
-            where: { nombre: nombre },
+            where: { id_usuarios: id },
         });
-        return NextResponse.json({ message: result }, { status: 200 });
-    } catch (errror) {
-        return new NextResponse(error.menssage, {status: 500})
+        return NextResponse.json({ message: result }, { status: 200 })
+    } catch (error) {
+        return new NextResponse(error.menssage, {status: 500});
     }
 }
 
 export async function PUT(request, { params }) {
-    console.log(params.id)
-    const nombre = params.id
-    const data = await request.json();
+    const id = parseInt(params.id);
+    const inputData = await request.json();
+    const data = {};
+
+    // Condicionales para agregar solo campos no vacíos
+    if (inputData.nombre && inputData.nombre.trim() !== "") {
+        data.nombre = inputData.nombre;
+    }
+
+    if (inputData.correo && inputData.correo.trim() !== "") {
+        data.correo = inputData.correo;
+    }
+
+    if (inputData.contrasena && inputData.contrasena.trim() !== "") {
+        data.contrasena = inputData.contrasena;
+    }
 
     try {
         const result = await prisma.usuario.update({
-            where: { nombre: nombre },
+            where: { id_usuarios: id },
             data: data,
         });
 
         if (!result) {
-            return new NextResponse("Usuario NO Encontrado", { status: 404,});
+            return new NextResponse("Usuario NO Encontrado", { status: 404 });
         }
-        return NextResponse.json({message:result}, {status:200})
+        return NextResponse.json({ message: result }, { status: 200 });
     } catch (error) {
-        return new NextResponse(error.message, {status: 500})
+        return new NextResponse(error.message, { status: 500 });
     }
 }
