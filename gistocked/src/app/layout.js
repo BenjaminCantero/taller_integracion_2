e10444cba9ed239c611c237eb630a8a26f2ffe12
oscarbarra <135163378+oscarbarra@users.noreply.github.com>
2024-10-05 00:27:47 -0300

    usuarios estaticos implementados

--- gistocked/src/app/layout.js
+++ gistocked/src/app/layout.js
@@ -3,6 +3,8 @@
 import Layout from '@/components/layout';
 import './globals.css';
 
+import { UserProvider } from './globalsUsers';
+
 export const metadata = {
   title: 'Gistoked',
   description: 'Zona de ventas',
@@ -12,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body>
-        <Layout>
-          {children}
-        </Layout>
+        <UserProvider>
+          <Layout>
+            {children}
+          </Layout>
+        </UserProvider>
       </body>
     </html>
   );
