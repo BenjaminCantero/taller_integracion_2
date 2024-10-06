5de1d693f66e4ffaa3336274d82665b81aa29be6
Gerald-herrerab <134649587+Gerald-herrerab@users.noreply.github.com>
2024-10-06 16:38:58 -0300

    update eliminar producto

--- gistocked/src/app/sales/venta.js
+++ gistocked/src/app/sales/venta.js
@@ -4,69 +4,100 @@ import React, { useState, useEffect } from 'react';
 const Venta = () => {
   const [productos, setProductos] = useState([
     { id: 1, nombre: 'Guantes Quirúrgicos', precio: 1500, stock: 50 },
-    { id: 2, nombre: 'Mascarillas N95', precio: 2500, stock: 50 },
-    { id: 3, nombre: 'Jeringas 5ml', precio: 500, stock: 10 },
+    { id: 2, nombre: 'Mascarillas N95'    , precio: 2500, stock: 50 },
+    { id: 3, nombre: 'Jeringas 5ml'       , precio: 500 , stock: 10 },
   ]);
 
   const [tablaVentas, setTablaVentas] = useState({});
   const [total, setTotal] = useState(0);
   const [codigoBarras, setCodigoBarras] = useState('');
-  const [escanerVisible, setEscanerVisible] = useState(false);
+  const [escanerVisible, setEscanerVisible] = useState(false); 
   const [productosSeleccionados, setProductosSeleccionados] = useState([]);
-
+  
   const [formaPago, setFormaPago] = useState('');
   const [tipoComprobante, setTipoComprobante] = useState('');
 
+  {/* ---------------------------------------------------------------------------- */}
+  {/* ---------------------------------------------------------------------------- */}
+  {/* ---------------------------------------------------------------------------- */}
+
   const agregarProductoAVenta = (producto) => {
     if (producto.stock > 0) {
-      let copiaProducto = { ...producto };
+      let copiaProducto = { ...producto }
       copiaProducto.cantidad = 0;
 
       if (copiaProducto.cantidad < copiaProducto.stock) {
-        setTablaVentas((prev) => {
-          const nuevoTotal = prev[copiaProducto.nombre] ? prev[copiaProducto.nombre].cantidad + 1 : 1;
-
-          // Verificamos que el nuevo total no exceda el stock
-          if (nuevoTotal <= copiaProducto.stock) {
-            const nuevoPrecio = prev[copiaProducto.nombre] ? prev[copiaProducto.nombre].precio + copiaProducto.precio : copiaProducto.precio;
-
-            return {
-              ...prev,
-              [copiaProducto.nombre]: {
-                ...copiaProducto,
-                cantidad: nuevoTotal,
-                precio: nuevoPrecio,
-              },
-            };
-          } else {
-            console.log('No se puede agregar más productos, stock insuficiente.');
-            return prev;
-          }
+        setTablaVentas(prev => {
+            const nuevoTotal = prev[copiaProducto.nombre] ? prev[copiaProducto.nombre].cantidad + 1 : 1;
+            
+            // Verificamos que el nuevo total no exceda el stock
+            if (nuevoTotal <= copiaProducto.stock) {
+                const nuevoPrecio = prev[copiaProducto.nombre] ? prev[copiaProducto.nombre].precio + copiaProducto.precio : copiaProducto.precio;
+    
+                return {
+                    ...prev,
+                    [copiaProducto.nombre]: {
+                        ...copiaProducto,
+                        cantidad: nuevoTotal,
+                        precio: nuevoPrecio
+                    }
+                };
+            } else {
+                // Opcional: puedes manejar el caso en que no se pueda agregar más
+                console.log('No se puede agregar más productos, stock insuficiente.');
+                return prev; // Retorna el estado previo si no se puede agregar
+            }
         });
-      }
-    } else {
-      console.log('Producto sin stock');
     }
 
+    } else {
+      console.log("Producto sin stock")
+    }
+    
+    
     setTotal(total + producto.precio);
   };
 
-  const eliminarProductoDeVenta = (nombreProducto) => {
-    setTablaVentas((prev) => {
-      const { [nombreProducto]: producto, ...resto } = prev;
-      if (producto) {
-        setTotal((prevTotal) => prevTotal - producto.precio);
+  const retirarProductosSeleccionados = () => {
+    let nuevoTotal = total;
+    const nuevosProductos = { ...tablaVentas };
+
+    productosSeleccionados.forEach(index => {
+      const nombre = Object.keys(nuevosProductos)[index];
+      if (nuevosProductos[nombre]) {
+        nuevoTotal -= nuevosProductos[nombre].precio;
+        delete nuevosProductos[nombre];
       }
-      return resto;
     });
+
+    setTablaVentas(nuevosProductos);
+    setTotal(nuevoTotal);
+    setProductosSeleccionados([]); 
   };
+
+  const venderProducto = () => {
+    const copiaProductos = { ...productos }
+    const arrayCopiaProductos = Object.values(copiaProductos);
+
+    const productosEnVenta = Object.values(tablaVentas);
+    const cantidadProductosEnVenta = Object.keys(tablaVentas).length;
+
+    for (let i = 0; i < cantidadProductosEnVenta; i++) {
+      arrayCopiaProductos[productosEnVenta[i].id -1].stock -= productosEnVenta[i].cantidad;
+    }
+
+    console.log(arrayCopiaProductos);
+    setProductos(arrayCopiaProductos);
+    setTablaVentas([]);
+  };
+
 
   const manejarEscaneo = (e) => {
     if (e.key === 'Enter' && codigoBarras.trim() !== '') {
-      const producto = productos.find((p) => p.id.toString() === codigoBarras);
+      const producto = productos.find(p => p.id.toString() === codigoBarras);
       if (producto) {
         agregarProductoAVenta(producto);
-        setCodigoBarras('');
+        setCodigoBarras('');  
       } else {
         alert('Producto no encontrado.');
       }
@@ -103,10 +134,10 @@ const Venta = () => {
             className="border border-gray-300 rounded-lg p-3 w-full mr-4"
             value={codigoBarras}
             onChange={(e) => setCodigoBarras(e.target.value)}
-            onKeyDown={manejarEscaneo}
+            onKeyDown={manejarEscaneo} 
           />
           <button className="bg-gray-200 p-3 rounded-lg text-black">Escáner QR</button>
-          <button
+          <button 
             className="ml-2 bg-gray-200 p-3 rounded-lg text-black"
             onClick={() => setEscanerVisible(!escanerVisible)}
           >
@@ -115,7 +146,7 @@ const Venta = () => {
         </div>
 
         {/* Menú desplegable para escanear */}
-         {escanerVisible && (
+        {escanerVisible && (
           <div className="bg-white p-6 rounded-lg shadow-lg mb-6">
             <h2 className="font-bold text-lg mb-4">Escanear Código de Barras</h2>
             <input
@@ -124,18 +155,25 @@ const Venta = () => {
               className="border border-gray-300 rounded-lg p-3 w-full"
               value={codigoBarras}
               onChange={(e) => setCodigoBarras(e.target.value)}
-              onKeyDown={manejarEscaneo}
+              onKeyDown={manejarEscaneo} 
             />
           </div>
         )}
+
+        {/* ---------------------------------------------------------------------------- */}
+        {/* ---------------------------------------------------------------------------- */}
+        {/* ---------------------------------------------------------------------------- */}
 
         {/* Productos disponibles */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10 w-full">
           {productos.map((producto) => (
             <div key={producto.id} className="bg-white p-6 rounded-lg shadow-md">
+
               <h3 className="text-xl font-bold mb-2">{producto.nombre}</h3>
+
               <p className="text-gray-700">Precio: ${producto.precio}</p>
               <p className="text-gray-700">Cantidad en stock: {producto.stock}</p>
+              
               <button
                 className="mt-4 bg-blue-500 text-white p-2 rounded-lg"
                 onClick={() => agregarProductoAVenta(producto)}
@@ -143,6 +181,7 @@ const Venta = () => {
                 Agregar a la venta
               </button>
             </div>
+
           ))}
         </div>
 
@@ -155,11 +194,10 @@ const Venta = () => {
                 <th className="px-4 py-2">Producto</th>
                 <th className="px-4 py-2">Cantidad</th>
                 <th className="px-4 py-2">Precio</th>
-                <th className="px-4 py-2">Acciones</th>
               </tr>
             </thead>
             <tbody>
-              {Object.entries(tablaVentas).map(([nombre, producto]) => (
+              {Object.entries(tablaVentas).map(([nombre, producto], index) => (
                 <tr key={nombre}>
                   <td className="border px-4 py-2">{nombre}</td>
                   <td className="border px-4 py-2">{producto.cantidad}</td>
@@ -182,19 +220,19 @@ const Venta = () => {
             <h3 className="text-xl font-bold mb-2">Forma de Pago:</h3>
             <div className="flex space-x-4">
               <button
-                className={`p-2 rounded-lg ${formaPago === 'Efectivo' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'}`}
+                className={"p-2 rounded-lg ${formaPago === 'Efectivo' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'}"}
                 onClick={() => seleccionarFormaPago('Efectivo')}
               >
                 Efectivo
               </button>
               <button
-                className={`p-2 rounded-lg ${formaPago === 'Tarjeta' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'}`}
+                className={"p-2 rounded-lg ${formaPago === 'Tarjeta' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'}"}
                 onClick={() => seleccionarFormaPago('Tarjeta')}
               >
                 Tarjeta
               </button>
               <button
-                className={`p-2 rounded-lg ${formaPago === 'Transferencia' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'}`}
+                className={"p-2 rounded-lg ${formaPago === 'Transferencia' ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'}"}
                 onClick={() => seleccionarFormaPago('Transferencia')}
               >
                 Transferencia
@@ -207,13 +245,13 @@ const Venta = () => {
             <h3 className="text-xl font-bold mb-2">Tipo de Comprobante:</h3>
             <div className="flex space-x-4">
               <button
-                className={`p-2 rounded-lg ${tipoComprobante === 'Boleta' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'}`}
+                className={"p-2 rounded-lg ${tipoComprobante === 'Boleta' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'}"}
                 onClick={() => seleccionarComprobante('Boleta')}
               >
                 Boleta
               </button>
               <button
-                className={`p-2 rounded-lg ${tipoComprobante === 'Factura' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'}`}
+                className={"p-2 rounded-lg ${tipoComprobante === 'Factura' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'}"}
                 onClick={() => seleccionarComprobante('Factura')}
               >
                 Factura
@@ -227,8 +265,8 @@ const Venta = () => {
               <h3 className="text-xl font-bold">Total: ${total}</h3>
             </div>
 
-            <div className="inline-block mt-6 text-right ml-4">
-              <button onClick={() => console.log('Vender')} className="bg-blue-500 text-white p-2 rounded-lg">
+            <div className="inline-block mt-6 text-right ml-4"> {/* Agregar margen a la izquierda para separar */}
+              <button onClick={venderProducto} className="bg-blue-500 text-white p-2 rounded-lg">
                 Vender
               </button>
             </div>
